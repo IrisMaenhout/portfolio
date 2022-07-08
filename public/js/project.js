b5493bb41611c035f4bc2b06d5802fa6475a20f7
IrisMaenhout <irismaen@student.arteveldehs.be>
2022-07-08 17:28:50 +0200

    finished working on project page layout

--- public/js/project.js
+++ public/js/project.js
@@ -4,18 +4,27 @@ var __webpack_exports__ = {};
   !*** ./resources/js/project.js ***!
   \*********************************/
 // Show or hide sidebar with extra information about the project if the user scrolls up or down.
-var sidebar = document.querySelector('.info-sidebar');
-window.addEventListener('resize', function () {
-  if (window.innerWidth >= 750) {
-    window.addEventListener('scroll', function (e) {
-      if (window.pageYOffset >= 20) {
-        sidebar.style.right = '0';
-        console.log(window.innerWidth);
-      } else {
-        sidebar.style.right = '-100vw';
-        console.log(window.innerWidth);
-      }
-    });
+var sidebar = document.querySelector('.info-sidebar'); // window.addEventListener('resize', ()=>{
+//     if(window.innerWidth >= 750){
+//         window.addEventListener('scroll', function(e){
+//             if (window.pageYOffset >= 20) {
+//                 sidebar.style.right = '0';
+//                 console.log(window.innerWidth);
+//             }else{
+//                 sidebar.style.right = '-100vw';
+//                 console.log(window.innerWidth);
+//             }
+//         });
+//     }
+// });
+
+window.addEventListener('scroll', function (e) {
+  if (window.pageYOffset >= 20) {
+    sidebar.style.right = '0';
+    console.log(window.innerWidth);
+  } else {
+    sidebar.style.right = '-100vw';
+    console.log(window.innerWidth);
   }
 });
 /******/ })()
--- resources/views/project/project-info.blade.php
+++ resources/views/project/project-info.blade.php
@@ -2,8 +2,7 @@
     <p class="domain">Design & development</p>
     <h1>Samen uit, samen thuis</h1>
 
-    <p class="short-description">Applicatie om evenementen te plannen en zorgt er voor dat gebruikers zich veilig voelen
-        tijdens hun verplaatsing naar het evenement.</p>
+    <p class="short-description long-text">Applicatie om evenementen te plannen en zorgt er voor dat gebruikers zich veilig voelen tijdens hun verplaatsing naar het evenement.</p>
 
     <div class="project-img">
         <i class="fa-solid fa-chevron-left"></i>
@@ -27,5 +26,20 @@
         <a href="https://xd.adobe.com/view/f6a6a454-0fe7-4f00-8626-9669e841504d-a3f5/screen/951eb119-9f9b-4da1-b8e1-e646a5967be4/?hints=off" target="_blank">
             <i class="fi fi-brands-xd"></i>
         </a>
+    </div>
+
+    <div class="process long-text">
+        <h3>Process</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas suscipit consectetur tellus sit amet finibus. Curabitur velit turpis, congue non aliquet eu, commodo ac justo. Nullam ac gravida massa. Maecenas sit amet lacus placerat, auctor dolor id, vehicula nulla. Suspendisse ac felis rutrum, fringilla erat ut, pharetra purus. Cras risus dolor, vulputate ac malesuada ut, pharetra sed justo. Duis id interdum enim, sit amet tristique metus. Aliquam a accumsan magna, nec placerat purus.</p>
+
+        <p>Morbi volutpat risus ut augue rhoncus egestas. Phasellus varius finibus viverra. Duis condimentum massa a imperdiet tincidunt. Fusce rhoncus, diam nec rhoncus facilisis, enim erat laoreet enim, sed ultrices augue nulla nec justo. Suspendisse imperdiet blandit ante. Vivamus faucibus erat accumsan, ultricies elit vitae, finibus lacus. Donec non congue diam. Pellentesque scelerisque massa sit amet sapien tempor pellentesque.</p>
+    </div>
+
+    <div class="process-img"></div>
+
+    <h2>Vond je dit een interessant project?</h2>
+    <div class="project-main-btns">
+        <a class="secundary-btn" href="/#projects">Bekijk meer projecten</a>
+        <a class="primary-btn" href="/#contact">Contacteer mij</a>
     </div>
 </div>
